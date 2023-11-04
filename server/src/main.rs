588c0f8281541630729374e50fdcfd0be40eda2b
gcarvellas <gcarvel264@gmail.com>
2023-11-04 13:47:42 -0400

    added code

---
+++ server/src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{post, HttpServer, HttpResponse, App, Responder};
+
+#[post("/data")]
+async fn pots_data() impl Responder {
+    HttpResponse::Ok().body({})
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 3001;
+
+    HttpServer::new(|| {
+        App::new()
+            .service(post_data)
+    })
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
+}
