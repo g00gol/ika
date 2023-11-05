b9a002ee9c85cf5baaca1cbc4e5ca1fa034acc45
g00gol <jackey2870@gmail.com>
2023-11-05 04:59:02 -0500

    thigns

--- client/src/utils/geoJsonProcessor.js
+++ client/src/utils/geoJsonProcessor.js
@@ -10,20 +10,50 @@ const res = await fetch("http://127.0.0.1:3001/data", {
   }),
 });
 
-const format = async () => {
+const format = async (endYear) => {
   const { columns: data } = await res.json();
-  const countryCodes = data[4].values;
+  const changeRate = data.find((col) => col.name === "Change Per Year").values;
+  const countryCodes = data.find((col) => col.name === "Code").values;
 
-  const map = new Map();
-  const updated = countryCodes.map((code) => {
-    const { features } = countries;
+  // For each year till the endYear, look for that value in the data
+  const allYears = {};
+  for (let year = 2019; year <= endYear; year++) {
+    const found = data.find((col) => col.name === year.toString()).values;
+    allYears[year] = found;
+  }
 
-    features.map((feature) => {
-      if (feature?.properties?.ISO_A3 === code) {
-        return;
+  // Find the intersection between countryCodes and countries.features.properties.ISO_A3
+  // Returns the actual country codes
+  const toUpdate = countries.features
+    .map((country) => {
+      if (countryCodes.includes(country.properties.ISO_A3)) {
+        return country.properties.ISO_A3;
       }
-    });
+    })
+    .filter((code) => code !== undefined);
+
+  // country.properties.averageChangeRate = changeRate[index];
+  const updated = countryCodes.map((code, index) => {
+    if (toUpdate.includes(code)) {
+      return {
+        ...countries.features.find(
+          (country) => country.properties.ISO_A3 === code,
+        ),
+        properties: {
+          ...countries.features.find(
+            (country) => country.properties.ISO_A3 === code,
+          ).properties,
+          averageChangeRate: changeRate[index],
+          // spread out all the years but the value is at an index
+          ...Object.fromEntries(
+            Object.entries(allYears).map(([key, value]) => [key, value[index]]),
+          ),
+        },
+      };
+    }
   });
+
+  console.log(updated);
 };
 
-format();
+format(2023);
