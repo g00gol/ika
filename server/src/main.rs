46859ef49a3cae75d1debbd001c34e8693919e66
gcarvellas <gcarvel264@gmail.com>
2023-11-05 02:01:37 -0500

    Your mother

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,42 +1,50 @@
 mod init;
 
 use std::sync::Mutex;
-use actix_web::{web, HttpServer, HttpResponse, App, Responder};
+use actix_web::{web, Error, post, HttpResponse, Result, HttpServer, App, Responder};
 use polars::prelude::*;
 use serde::{Serialize, Deserialize};
+use serde_json;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct PostBody {
     year: u64
 }
 
-async fn post(body: web::Json<PostBody>, df: web::Data<Mutex<DataFrame>>) -> impl Responder {
+#[post("/data")]
+async fn post_route(body: web::Json<PostBody>, df: web::Data<Mutex<DataFrame>>) -> Result<HttpResponse, Error> {
 
+    // Append columns from 2020 until the input year
     let expressions: Vec<Expr> = (2020..(body.year+1))
-        .map(|y| col("2019 Total Emitted")
-                      .alias(format!("{} Total Emitted", y).as_str())
-                      +col("Change Per Year")*lit(y-2019)
-                      ).collect();
 
+        // Prevent future Total Emitted being less than 0 
+        .map(|y| when((col("2019")+col("Change Per Year")*lit(y-2019)).lt(0))
 
-    println!("{:?}", expressions);
+             .then(lit(0).alias(format!("{}", y).as_str()))
+             .otherwise(
+                 col("2019")
+                          .alias(format!("{}", y).as_str())
+                          +col("Change Per Year")*lit(y-2019)
+             )).collect();
 
     // We put the hack in hackathon
     let lf_years_emitted: LazyFrame = df.lock().unwrap().clone().lazy()
                                         .select(&[
                                            col("Entity"),
                                            col("Continent"),
-                                           col("2019 Total Emitted"),
+                                           col("2019 Total Emitted").alias("2019"),
                                            col("Change Per Year")
                                         ])
                                         .with_columns(
                                             expressions
-                                            )
-                                        ;
+                                            );
 
-    println!("{}", lf_years_emitted.clone().collect().unwrap());
+    Ok(HttpResponse::Ok().json(serde_json::to_value(lf_years_emitted.clone().collect().unwrap())?))
 
-    HttpResponse::Ok().body({})
+   // Ok(web::Json(
+     //       lf_years_emitted.clone().collect().unwrap()
+       //     )
+        //)
 }
 
 #[actix_web::main]
@@ -48,7 +56,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(Mutex::new(df.clone()))) 
-            .service(web::resource("/data").route(web::post().to(post)))
+            .service(post_route)
+            //.service(web::resource("/data").route(web::post().to(post)))
     })
     .bind(("0.0.0.0", port))?
     .run()
