3153ce32e49a688b935115af9b37f542e02f6647
Matt Turner <mdturner1026@gmail.com>
2023-11-04 20:53:56 -0400

    Added emit prob to frame and added dataset for total gen per country

--- server/src/bin/data.rs
+++ server/src/bin/data.rs
@@ -7,31 +7,33 @@ fn main() -> () {
 
     let lf_country_pop: LazyFrame = LazyCsvReader::new("./data/Country_Population_Data.csv").finish().unwrap();
 
-    // let lf_gen_region: LazyFrame = LazyCsvReader::new("./data/plastic_waste_generation/total.csv").finish().unwrap();
-    let lf_gen_country_capita: LazyFrame = LazyCsvReader::new("./data/plastic_waste_generation/per_capita.csv").finish().unwrap();
+    // let lf_gen_region: LazyFrame = LazyCsvReader::new("./data/plastic_waste_generation/total_region.csv").finish().unwrap();
+    let lf_gen_country: LazyFrame = LazyCsvReader::new("./data/plastic_waste_generation/total_country.csv").finish().unwrap();
 
     let lf_mis_region: LazyFrame = LazyCsvReader::new("./data/mismanaged_waste/total.csv").finish().unwrap();
     let lf_mis_country: LazyFrame = LazyCsvReader::new("./data/mismanaged_waste/per_capita.csv").finish().unwrap();
 
-    // let lf_prob_country: LazyFrame = LazyCsvReader::new("./data/probability_of_ocean_pollution/total.csv").finish().unwrap();
+    let lf_prob_country: LazyFrame = LazyCsvReader::new("./data/probability_of_ocean_pollution/total.csv").finish().unwrap();
 
     let lf_emit_capita: LazyFrame = LazyCsvReader::new("./data/plastic_emitted_to_ocean/per_capita.csv").finish().unwrap();
     // let lf_emit_total: LazyFrame = LazyCsvReader::new("./data/plastic_emitted_to_ocean/total.csv").finish().unwrap();
     
-    let lf_2010_country: LazyFrame = lf_gen_country_capita.clone()
-                                    .join_builder()
-                                        .with(lf_country_pop.clone())
-                                        .how(JoinType::Inner)
-                                        .left_on(&[col("Entity")])
-                                        .right_on(&[col("Country Name")])
-                                        .finish()
-                                    .select(&[col("Entity"),
-                                              col("Code"),
-                                              col("Per capita plastic waste (kg/person/day)").alias("2010 Total Waste Generated") * col("2010")]);
+
+    // let 
+
+    let lf_2010_country: LazyFrame = lf_gen_country.left_join(lf_countries_continents,
+                                                              col("Entity"),
+                                                              col("Country"))
+                                                 // We probably lost some countries here because of the join
+                                                   .select(&[col("Entity"),
+                                                             col("Code"),
+                                                             col("Continent"),
+                                                             col("Plastic waste generation (tonnes, total)").alias("2010 Total Waste Generated") * lit(1000)]);
     
     let lf_2019_country: LazyFrame = lf_mis_country
                                      .join_builder()
                                          .with(lf_emit_capita)
+                                         .how(JoinType::Inner)
                                          .on(&[col("Entity"),
                                                col("Year")])
                                          .finish()
@@ -45,30 +47,25 @@ fn main() -> () {
                                          .left_on(&[col("Entity")])
                                          .right_on(&[col("Country Name")])
                                          .finish()
+                                     .inner_join(lf_prob_country,
+                                                 col("Entity"),
+                                                 col("Entity"))
                                      .select(&[col("Entity"),
                                                col("Code"),
                                                col("Mismanaged per capita").alias("2019 Total Mismanaged") * col("2019"),
-                                               col("Emit per capita").alias("2019 Total Emitted") * col("2019")]);
+                                               col("Emit per capita").alias("2019 Total Emitted") * col("2019"),
+                                               col("Probability of plastic being emitted to ocean").alias("2019 Prob Emitted")]);
 
-    let lf_country: LazyFrame = lf_2010_country.inner_join(lf_2019_country,
+    let lf_country: LazyFrame = lf_2010_country.outer_join(lf_2019_country,
                                                            col("Entity"),
                                                            col("Entity"))
-                                            // We probably lost some countries here because of the join
-                                               .left_join(lf_countries_continents,
-                                                           col("Entity"),
-                                                           col("Country"))
-                                               .filter(col("Continent").is_null())
                                                .select(&[col("Entity"),
                                                          col("Code"),
                                                          col("Continent"),
                                                          col("2010 Total Waste Generated"),
                                                          col("2019 Total Mismanaged"),
-                                                         col("2019 Total Emitted")]);
-
-    let lf_gen_countries: LazyFrame = lf_mis_region.filter(
-            col("Year").eq(lit(2019))
-            .or(col("Year").eq(lit(2010))));
-    println!("{}", lf_gen_countries.collect().unwrap());
+                                                         col("2019 Total Emitted"),
+                                                         col("2019 Prob Emitted")]);
 
     let lf1: DataFrame = lf_country.collect().unwrap();
     println!("{}", lf1);
