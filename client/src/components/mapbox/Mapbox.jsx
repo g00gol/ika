2d5fbe8c96fee2b3baf650021bb18364ae115b6a
KevinHa48 <kh220kh@gmail.com>
2023-11-05 07:26:56 -0500

    e

--- client/src/components/mapbox/Mapbox.jsx
+++ client/src/components/mapbox/Mapbox.jsx
@@ -7,14 +7,40 @@ const API_KEY = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
 export default function Mapbox() {
   const [geoJson, setGeoJson] = useState(null);
+  const [maxValue, setMaxValue] = useState(0);
 
   useEffect(() => {
     (async () => {
       const data = await format(2023);
-      console.log(data);
+      const max = Math.max(
+        ...data.features.map((country) => country.properties["2023"]),
+      );
+      setMaxValue(max);
       setGeoJson(data);
     })();
   }, []);
+
+  const parkLayer = {
+    id: "keepyourselfsafe",
+    type: "fill",
+    source: "2023",
+    paint: {
+      "fill-color": [
+        "interpolate",
+        ["linear"],
+        ["get", "2023"],
+        0,
+        "#F2F12D",
+        maxValue / 4,
+        "#E6B71E",
+        maxValue / 2,
+        "#8B4225",
+        maxValue,
+        "#723122",
+      ],
+      "fill-opacity": 0.75,
+    },
+  };
 
   return (
     <div className="h-screen w-screen">
@@ -29,7 +55,9 @@ export default function Mapbox() {
         style={{ position: "relative", width: "100%", height: "100%" }}
         mapStyle="mapbox://styles/tumtarts/clokif760006001nt96303gcu"
       >
-        <Source id="my-data" type="geojson" data={geoJson}></Source>
+        <Source id="my-data" type="geojson" data={geoJson}>
+          <Layer {...parkLayer} />
+        </Source>
       </Map>
     </div>
   );
