b500ffd539da81c7178b1ca27f3cee5978bccc2a
gcarvellas <gcarvel264@gmail.com>
2023-11-05 06:21:58 -0500

    cleanup cors stuff

--- server/src/main.rs
+++ server/src/main.rs
@@ -28,7 +28,6 @@ async fn post_route(body: web::Json<PostBody>, df: web::Data<Mutex<DataFrame>>) 
                           +col("Change Per Year")*lit(y-2019)
              )).collect();
 
-    // We put the hack in hackathon
     let lf_years_emitted: LazyFrame = df.lock().unwrap().clone().lazy()
                                         .select(&[
                                            col("Entity"),
@@ -41,12 +40,12 @@ async fn post_route(body: web::Json<PostBody>, df: web::Data<Mutex<DataFrame>>) 
                                             expressions
                                             );
 
-    Ok(HttpResponse::Ok()
-       // We put the hack in hackathon
-        .append_header(("Access-Control-Allow-Headers", "*"))
-        .append_header(("Access-Control-Allow-Origin", "*"))
-        .append_header(("Access-Control-Allow-Methods", "*"))
-       .json(serde_json::to_value(lf_years_emitted.collect().unwrap())?))
+    Ok(
+        HttpResponse::Ok()
+        .json(
+            serde_json::to_value(lf_years_emitted.collect().unwrap())?
+            )
+      )
 
 }
 
@@ -58,21 +57,16 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         let cors = Cors::default()
-            .allowed_origin("http://localhost:3001")
             .allowed_origin("http://localhost:5173")
-            .allowed_origin_fn(|origin, _req_head| {
-                origin.as_bytes().ends_with(b".rust-lang.org")
-             })
-              .allowed_methods(vec!["GET", "POST", "OPTION"])
-              .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-              .allowed_header(http::header::CONTENT_TYPE)
-              .max_age(3600);
+            .allowed_methods(vec!["GET", "POST", "OPTION"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
 
         App::new()
             .wrap(cors)
             .app_data(web::Data::new(Mutex::new(df.clone()))) 
             .service(post_route)
-            //.service(web::resource("/data").route(web::post().to(post)))
     })
     .bind(("0.0.0.0", port))?
     .run()
